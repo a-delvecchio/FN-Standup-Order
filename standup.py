2e3ba1c153e05bd78f9ddf160d92861e8baa4fc7
a-delvecchio <alex.delvecchio@fiscalnote.com>
2018-06-18 23:20:36 +0000

    Bug fixes

--- standup.py
+++ standup.py
@@ -4,23 +4,20 @@ import random
 
 
 def main():
-    """
-    Main CLI function
-    """
     parser = argparse.ArgumentParser(description='Standup Order Chooser')
-    parser.add_argument('--absent', '-a',
-                        help='Specify who is absent from standup',
-                        action='store')
+    parser.add_argument('--absent', '-a', nargs='+', help='Specify who is absent from standup', action='store')
     args = parser.parse_args()
-    absent = args.absent.lower() if args.absent else None
+    absent = [name.lower() for name in args.absent] if args.absent else None
     with open('config.json') as json_file:
         config = json.load(json_file)
-
-    present = [member for member in config['members'] if not absent or member not in absent]
-    start_with = random.choice(present)
-    direction = random.choice(['left', 'right'])
-
-    print "Start with {} and go {}".format(start_with, direction)
+    members = [member.lower() for member in config['members']]
+    present = [member for member in members if member not in absent] if absent else members
+    if present:
+        start_with = random.choice(present).capitalize()
+        direction = random.choice(['left', 'right'])
+        print "Start with {} and go {}".format(start_with, direction)
+    else:
+        print "No one is at standup, so it doesn't really matter what the order is..."
 
 
 if __name__ == "__main__":
